3087ea6ca1246b1b76c3a8bec9cab6d120570eb8
njoel24 <cristiano.anselmi@gmail.com>
2017-06-16 23:57:43 +0200

    feat: add page search

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
+var request = require('request-promise');
 
+var bodyParser = require('body-parser');
+
+console.log("server start");
+
+
+var payload = {  
+  queryTerm: 'Fiat',
+  searchType: 'page'
+}
+
+var userToken = "";
 
 // Configure the Facebook strategy for use by Passport.
 //
@@ -10,10 +22,11 @@ var Strategy = require('passport-facebook').Strategy;
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
+// access Token : EAAbBwxujROQBAEwzCCoryetMTZBZCpzVqAcKwbikbsMZCrZA5rWUnjWbpDafspcfPTwqGZC3xuZAdjdrFT1LCEl193akFcNAMsYHDPgGohYIh8EbGNYuvz3SDYTVs2iyWPxb4Y67kl6CUgL8zRyQUSUXREhbrWO7wZD
 passport.use(new Strategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/login/facebook/return'
+    clientID: 1901893586732260,
+    clientSecret: '3dcfddb16a15c9550a3c8b0a93a246b9',
+    callbackURL: '/login/facebook/return'
   },
   function(accessToken, refreshToken, profile, cb) {
     // In this example, the user's Facebook profile is supplied as the user
@@ -21,6 +34,8 @@ passport.use(new Strategy({
     // be associated with a user record in the application's database, which
     // allows for account linking and authentication with other identity
     // providers.
+    userToken = accessToken;
+    console.log("ecco l'accessToken:"+accessToken)
     return cb(null, profile);
   }));
 
@@ -45,6 +60,11 @@ passport.deserializeUser(function(obj, cb) {
 
 // Create a new Express application.
 var app = express();
+
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // Configure view engine to render EJS templates.
 app.set('views', __dirname + '/views');
@@ -75,7 +95,7 @@ app.get('/login',
   });
 
 app.get('/login/facebook',
-  passport.authenticate('facebook'));
+  passport.authenticate('facebook', { scope: ['publish_actions', 'manage_pages'] }));
 
 app.get('/login/facebook/return', 
   passport.authenticate('facebook', { failureRedirect: '/login' }),
@@ -89,4 +109,67 @@ app.get('/profile',
     res.render('profile', { user: req.user });
   });
 
+app.post('/facebook-search', (req, res) => {
+  var userFieldSet = 'name, link, is_verified, picture';
+  var pageFieldSet = 'name, category, link, picture, is_verified';
+  // var queryTerm, searchType  = req.body;
+  console.log("req: "+req.body.search);
+  var options = {
+    method: 'GET',
+    uri: 'https://graph.facebook.com/search',
+    qs: {
+      access_token: userToken,
+      q: req.body.search,
+      type: req.body.type,
+      fields: req.body.type === 'page' ? pageFieldSet : userFieldSet
+    }
+  };
+
+  request(options)
+      .then(fbRes => {
+    // Search results are in the data property of the response.
+    // There is another property that allows for pagination of results.
+    // Pagination will not be covered in this post,
+    // so we only need the data property of the parsed response.
+        var parsedRes = JSON.parse(fbRes).data; 
+        res.json(parsedRes);
+      })
+});
+
+
+app.get('/facebook-search/:id', (req, res) => {
+
+  // you need permission for most of these fields
+  var userFieldSet = 'id, name, about, email, accounts, link, is_verified, significant_other, relationship_status, website, picture, photos, feed';
+
+  var options = {
+    method: 'GET',
+    uri: `https://graph.facebook.com/v2.8/${req.params.id}`,
+    qs: {
+      access_token: user_access_token,
+      fields: userFieldSet
+    }
+  };
+
+  request(options)
+    .then(fbRes => {
+      res.json(fbRes);
+    })
+});
+
+function postContent() {
+  var id = 'page or user id goes here';  
+  var access_token = 'for page if posting to a page, for user if posting to a user\'s feed';
+
+  var postTextOptions = {  
+    method: 'POST',
+    uri: `https://graph.facebook.com/v2.8/${id}/feed`,
+    qs: {
+      access_token: access_token,
+      message: 'Hello world!'
+    }
+  };
+
+  request(postTextOptions); 
+}
 app.listen(3000);
